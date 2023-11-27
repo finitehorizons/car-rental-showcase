0c429c8b6ed2fe6dc0e6d127260a7c8802fc5656
christopher goodwin <goodwin.chris2021@gmail.com>
2023-11-27 13:41:22 -0600

    initial folder structure, hero section and basic search filtering complete

---
+++ types/index.ts
@@ -0,0 +1,14 @@
+import { manufacturers } from "./../constants/index";
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}

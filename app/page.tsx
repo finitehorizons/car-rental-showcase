9d8fc58596116baf97bbbe0be9db86c9843ff3e9
christopher goodwin <goodwin.chris2021@gmail.com>
2023-11-27 23:58:00 -0600

    API calls for Car Cards, set up structure and style of cards with temporary data

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,45 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default function Home() {
-    return <main className='overflow-hidden'>
-        <Hero />
-        <div className="mt-12 padding-x padding-y max-width" id="discover">
-            <div className="home__text-container">
-                <h1 className="text-4xl font-extrabold"> Car Catalogue</h1>
-                <p>Find your next dream vehicle</p>
-            </div>
+export default async function Home() {
+    const allCars = await fetchCars();
 
-            <div className="home__filters">
-                <SearchBar />
+    const isDataEmpty = !Array.isArray(allCars || allCars.length < 1 || !allCars);
 
-                <div className="home__filter-container">
-                    <CustomFilter title=""/>
-                    <CustomFilter title="year12312312" />
+    return (
+        <main className='overflow-hidden'>
+            <Hero />
+            <div className='mt-12 padding-x padding-y max-width' id='discover'>
+                <div className='home__text-container'>
+                    <h1 className='text-4xl font-extrabold'> Car Catalogue</h1>
+                    <p>Find your next dream vehicle</p>
                 </div>
+
+                <div className='home__filters'>
+                    <SearchBar />
+
+                    <div className='home__filter-container'>
+                        <CustomFilter title='' />
+                        <CustomFilter title='year12312312' />
+                    </div>
+                </div>
+
+                {!isDataEmpty ? (
+                    <section>
+                        <div className='home__cars-wrapper'>
+                            {allCars?.map((car) => (
+                                <CarCard car={car} />
+                            ))}
+                        </div>
+                    </section>
+                ) : (
+                    <div className='home__error-container'>
+                        <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+                        <p>{allCars?.message}</p>
+                    </div>
+                )}
             </div>
-        </div>
-    </main>;
+        </main>
+    );
 }
--- components/index.ts
+++ components/index.ts
@@ -5,5 +5,6 @@ import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
 
-export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer };
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard };
